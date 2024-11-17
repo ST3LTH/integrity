27223740ef2745844720500e31b480e2e102e502
ST3LTH <marquardtcameron@gmail.com>
2024-11-16 22:31:09 -0500

    Update ITS4050 CodeReview.py

--- ITS4050 CodeReview.py
+++ ITS4050 CodeReview.py
@@ -6,13 +6,17 @@ USERS = {
     'user': 'userpassword'
 }
 @app.route('/')
+
+
 def home():
     return "Welcome to the vulnerable app! Go to /login?username=USERNAME&password=PASSWORD to login!"
 @app.route('/login')
+
+
 def login():
     username = request.args.get('username')
     password = request.args.get('password')
-        # Vulnerable SQL query simulation
+    # Vulnerable SQL query simulation
     query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
     response = "Login failed!"
     if username in USERS and USERS[username] == password:
@@ -20,4 +24,6 @@ def login():
         # Simulating the execution of a SQL query and returning the result
     return render_template_string('<h2>' + response + '</h2><br>Executed query: ' + query)
 if __name__ == '__main__':
+
+    
     app.run(debug=True)
